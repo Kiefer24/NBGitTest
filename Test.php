a422b93a96f12512381c37f53021a3a22f472329
Hanus, Tomas02 (uic52239) <uic52239@BYL0724W.cw01.contiwan.com>
2021-04-12 21:14:41 +0200

    Remove note, add constructor

--- Test.php
+++ Test.php
@@ -1,16 +1,9 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Test
- *
- * @author uic52239
- */
 class Test {
-    //put your code here
+    
+    public function __construct() {
+        
+    }
+    
 }
